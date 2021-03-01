8f97f8ead6de1aa8b6ae4a0d528b31f25bcdc7fa
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-01 12:21:20 -0300

    Criando uma variável de tipo numérica

--- src/main/java/PrimeiroPrograma.java
+++ src/main/java/PrimeiroPrograma.java
@@ -1,6 +1,11 @@
 public class PrimeiroPrograma {
 
     public static void main (String []args){
-        System.out.println("Meu Deus, meu Deus, eu sou um hacker!");
+
+        //Variável tem que ser inicializada, quer dizer tem que ter um valor.
+        //int é o tipo de variávle. Neste caso é um número inteiro
+        //O nome da variável é idade
+        int idade = 35;
+        System.out.println("A idade da pessoa é: " + idade);
     }
 }
