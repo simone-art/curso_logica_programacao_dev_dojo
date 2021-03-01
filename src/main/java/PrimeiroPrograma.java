bbc52b874d78d6db04f82bd2217241630d014686
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-01 09:45:57 -0300

    Trocada mensagem do system.out.println

--- src/main/java/PrimeiroPrograma.java
+++ src/main/java/PrimeiroPrograma.java
@@ -1,6 +1,6 @@
 public class PrimeiroPrograma {
 
     public static void main (String []args){
-        System.out.println("Hello World!");
+        System.out.println("Meu Deus, meu Deus, eu sou um hacker!");
     }
 }
