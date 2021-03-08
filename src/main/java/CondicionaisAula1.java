34c93918c487b2c1f5413ac675f953a6fc5b5351
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 16:24:20 -0300

    "Criado código pra testar if com boolean"

---
+++ src/main/java/CondicionaisAula1.java
@@ -0,0 +1,14 @@
+public class CondicionaisAula1 {
+    /*
+    Sintasis para condicionais : if(){}/
+    dentro do paréntesis do if vai a condicional, que deve ser true ou false;
+    dentro do ife vão estar os blocod de instrução que serão executados
+     */
+    public static void main(String[] args) {
+        boolean condicao = false;
+       if(condicao){
+           System.out.println("Dentro do if");
+       }
+        System.out.println("Fora do If");
+    }
+}
