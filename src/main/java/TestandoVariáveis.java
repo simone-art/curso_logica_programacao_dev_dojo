f43347075bbb6474d209b7d7f80d8119527a48fa
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 16:09:20 -0300

    "Criado código pra testar variáveis char, string e boolean"

---
+++ src/main/java/TestandoVariáveis.java
@@ -0,0 +1,14 @@
+public class TestandoVariáveis {
+    public static void main(String[] args) {
+        /*
+        Char é um caractere
+
+         */
+        char caractere = 65;
+        System.out.println(caractere);
+        String nome = "Aqui você pode chamar uma frase grande";
+        System.out.println(nome);
+        boolean condicao = false;
+        System.out.println(condicao);
+    }
+}
