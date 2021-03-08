4c15d7289479ec279b346517979d6fb1cc4d058d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-08 16:50:40 -0300

    "Criado código pra calcular a idade de uma pessoa e verificar se é adulto ou não"

---
+++ src/main/java/CalculoMaiorDeIdade.java
@@ -0,0 +1,16 @@
+public class CalculoMaiorDeIdade {
+    /*
+    Receba uma idade como entrada
+    Se idade for maior que 18, imprima "adulto"
+    Senão, imprima "Não adulto";
+     */
+    public static void main(String[] args) {
+        int idade = 15;
+        if(idade >= 18){
+            System.out.println("Adulto");
+        }else{
+            System.out.println("Ainda não é adulto");
+        }
+
+    }
+}
--- src/main/java/CondicionaisAula1.java
+++ src/main/java/CondicionaisAula1.java
@@ -3,12 +3,14 @@ public class CondicionaisAula1 {
     Sintasis para condicionais : if(){}/
     dentro do paréntesis do if vai a condicional, que deve ser true ou false;
     dentro do ife vão estar os blocod de instrução que serão executados
+    Se o if for executado, o else não e viceversa
      */
     public static void main(String[] args) {
-        boolean condicao = false;
+        boolean condicao = true;
        if(condicao){
            System.out.println("Dentro do if");
+       }else{
+           System.out.println("Fora do If");
        }
-        System.out.println("Fora do If");
     }
 }
