a5d1471dbf2d9063319734ace3166e66cfcabb64
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-15 11:13:54 -0300

    "Criado código pra calcular porcentagem imposto salário usando ie e else, variáveis tipo string, etc"

--- src/main/java/CalculoMaiorDeIdade.java
+++ src/main/java/CalculoMaiorDeIdade.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CalculoMaiorDeIdade {
     /*
     Receba uma idade como entrada
@@ -5,12 +7,20 @@ public class CalculoMaiorDeIdade {
     Senão, imprima "Não adulto";
      */
     public static void main(String[] args) {
-        int idade = 15;
+//        int idade = 15;
+//        if(idade >= 18){
+//            System.out.println("Adulto");
+//        }else{
+//            System.out.println("Ainda não é adulto");
+//        }
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite a sua idade: ");
+        int idade = scanner.nextInt();
         if(idade >= 18){
-            System.out.println("Adulto");
+            System.out.println("Você é Adulto");
         }else{
             System.out.println("Ainda não é adulto");
         }
-
     }
 }
---
+++ src/main/java/CalculoValorPorcentagemSalario2.java
@@ -0,0 +1,23 @@
+public class CalculoValorPorcentagemSalario2 {
+
+        //Dado um determinado salário, se o salário foi maior que 4.500,
+        //imprima 30% do valor. Senão imprima 15
+
+        public static void main(String[] args) {
+            double salario = 3500;
+            double resultado;
+            String porcentagem = "";
+
+            if (salario > 4500){
+                resultado = salario * 30 / 100;
+                porcentagem = "30%";
+            }else {
+                resultado = salario * 15 / 100;
+                porcentagem = "10%";
+
+            }
+            System.out.println("O valor do imposto de " + porcentagem + " do seu salário é: " + resultado);
+        }
+
+}
+
