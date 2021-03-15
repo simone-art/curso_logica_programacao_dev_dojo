f8778cb9d93aa0cdae1f783193a7bdd169b49abe
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-15 13:25:26 -0300

    "Criado código pra calcular se o número é par ou impar usando número digitado pelo usuário"

--- src/main/java/CalcularNumeroParOuImpar.java
+++ src/main/java/CalcularNumeroParOuImpar.java
@@ -6,13 +6,15 @@ import java.util.Scanner;
 
 public class CalcularNumeroParOuImpar {
     public static void main(String[] args) {
-         int numero1 = 21;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um número: ");
+        int numero1 = scanner.nextInt();
          int numero2 = 2;
          int resultado = numero1 % numero2;
          if(resultado == 0){
-             System.out.println("O número " + numero1 + " é par");
+             System.out.println("O número  é par");
          }else{
-             System.out.println("O número " + numero1 + " é impar");
+             System.out.println("O número  é impar");
          }
     }
 }
