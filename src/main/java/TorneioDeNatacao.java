e1c85da474a06c0d99290605dc4ca05a6d886977
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-03-25 18:10:28 -0300

    "Criado outro código que imprime nome, idade e a sua categoria pra participar de um torneio de natação"

--- src/main/java/TorneioDeNatacao.java
+++ src/main/java/TorneioDeNatacao.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
 
-public class TorneioDeNatacao {
+//Eu como usuário gostaría de ter o nome e a idade de participantes
+// de um torneio de natação onde o sistema imprimisse o seguinte:
+//Menor que 10 anos <Nome> participará da categoria infantil
+//Entre 11 a 15 anos <Nome> participará da categoria juvenil
+//Entre 16 a 19 anos <Nome> participará da categoria Pre-Adulto
+//Acima de 20 anos <Nome> participará da categoria adulto
+//Para que eu possa rápidamente classificar todos os participantes
 
-    //Eu como usuário gostaría de ter o nome e a idade de participantes
-    // de um torneio de natação onde o sistema imprimisse o seguinte:
-    //Menor que 10 anos <Nome> participará da categoria infantil
-    //Entre 11 a 15 anos <Nome> participará da categoria juvenil
-    //Entre 16 a 19 anos <Nome> participará da categoria Pre-Adulto
-    //Acima de 20 anos <Nome> participará da categoria adulto
-    //Para que eu possa rápidamente classificar todos os participantes
+public class TorneioDeNatacao {
 
     public static void main(String[] args) {
 
-        Scanner scanner= new Scanner(System.in);
-        System.out.println("Digite o seu nome:");
-        String nomes = scanner.nextLine();
+        String nomes = "Mariana";
+        int idades = 19;
 
-        System.out.println("Digite a sua idade:");
-        int idades = scanner.nextInt();
-
-
-          if(idades <= 10){
+          if( idades <= 10){
               System.out.println(nomes + ", você pertence a categoría infantil");
           }else if(idades > 10 && idades <= 15){
               System.out.println( nomes + ", você pertence a categoría juvenil");
