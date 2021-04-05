a21db4c62c7e80ae083a4c138ccbd3a0673f4a9d
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-05 08:06:28 -0300

    "Refactorado código que recebe dados do teclado"

--- src/main/java/RecebendoDadosDoteclado.java
+++ src/main/java/RecebendoDadosDoteclado.java
@@ -5,10 +5,9 @@ public class RecebendoDadosDoteclado {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Digite o seu nome: ");
         String nome = scanner.nextLine();
-        System.out.println("O nome digitado foi: " + nome);
         System.out.println("Digite a sua idade: ");
         int idade = scanner.nextInt();
-        System.out.println("A idade digitada foi: " + idade);
+        System.out.println("O nome e a idade digitada são: " + nome + "," + idade);
 
     }
 
