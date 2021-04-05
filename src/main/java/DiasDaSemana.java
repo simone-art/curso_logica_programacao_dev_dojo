faacff1f08ee6f00de12e67bd9f4cbd367e72744
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-05 12:44:52 -0300

    "Criado código usando switch em português

--- src/main/java/DiasDaSemana.java
+++ src/main/java/DiasDaSemana.java
@@ -1,21 +1,22 @@
 /*
 Receba um número inteiro do usuário e imprima o día da semana correspondente
+Sendo 1 Segunda Feira
  */
 
 public class DiasDaSemana {
     public static void main(String[] args) {
-        int dia = 7;
+        int dia = 3;
         String diaDaSemana;
         switch (dia){
-            case 1: diaDaSemana = "Lunes";
+            case 1: diaDaSemana = "Segunda Feira";
             break;
-            case 2: diaDaSemana = "Martes";
+            case 2: diaDaSemana = "Terça Feira";
             break;
-            case 3: diaDaSemana = "Miércoles";
+            case 3: diaDaSemana = "Quarta Feira";
             break;
-            case 4: diaDaSemana = "Jueves";
+            case 4: diaDaSemana = "Quinta Feira";
             break;
-            case 5: diaDaSemana = "Viernes";
+            case 5: diaDaSemana = "Sexta Feira";
             break;
             case 6: diaDaSemana = "Sábado";
             break;
@@ -23,6 +24,6 @@ public class DiasDaSemana {
             break;
             default:diaDaSemana = "Dia inválido";
         }
-        System.out.println("Dia de la semana es: " +diaDaSemana);
+        System.out.println("O día da semana é: " +diaDaSemana);
     }
 }
