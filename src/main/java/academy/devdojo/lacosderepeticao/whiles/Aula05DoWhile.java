a6b15f634b308f341f68fb92768d54d1a30969a8
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-14 15:13:29 -0300

    "Refactorado código usando DoWhile pra fazer um jogo "

--- src/main/java/academy/devdojo/lacosderepeticao/whiles/Aula05DoWhile.java
+++ src/main/java/academy/devdojo/lacosderepeticao/whiles/Aula05DoWhile.java
@@ -1,18 +1,26 @@
 package academy.devdojo.lacosderepeticao.whiles;
 
+import java.util.Scanner;
+
 /*
 Do While é um laço de repetição onde a condição vem depois da primeira execução
  */
 public class Aula05DoWhile {
     public static void main(String[] args) {
-
-        boolean condicao = false;
-        while(condicao){
-            System.out.println("Dentro do While");
-        }
+        int desejaContinuar = 1;
+        Scanner scanner = new Scanner(System.in);
         do {
-            System.out.println("Dentro do Do While");
-        } while (condicao);
+            System.out.println("Player 1: Digite um número do 1 até 10");
+            int num1 = scanner.nextInt();
+            System.out.println("Player 2: Digite um número do 1 até 10");
+            int num2 = scanner.nextInt();
+            System.out.println("Acertou " + (num1 == num2));
+            System.out.println("===========================================");
+            System.out.println("Deseja continuar jogando?");
+            System.out.println("1. Sim");
+            System.out.println("2. Não");
+            desejaContinuar = scanner.nextInt();
 
+        } while (desejaContinuar == 1);
     }
 }
