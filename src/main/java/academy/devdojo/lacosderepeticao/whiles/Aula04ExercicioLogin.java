6345ab7aaf39061d76844ecf82c890932f9f790f
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-14 14:10:09 -0300

    "Criado exercicio usando o While para exhibir acesso concedido ou negado ao usuário introducir login e senha"

---
+++ src/main/java/academy/devdojo/lacosderepeticao/whiles/Aula04ExercicioLogin.java
@@ -0,0 +1,28 @@
+package academy.devdojo.lacosderepeticao.whiles;
+
+import java.util.Scanner;
+
+public class Aula04ExercicioLogin {
+    public static void main(String[] args) {
+        //Ao usar a apalavra final, a variável passa a ser constante e seu valor não poderá ser trocado
+        final String login = "Todoroku";
+        final String password = "ReiDosPiratas";
+        boolean exibirTelaLogin = true;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while(exibirTelaLogin){
+            System.out.print("Digite seu login: ");
+            String loginDigitado = scanner.nextLine();
+            System.out.print("Digite seu password: ");
+            String passwordDigitado = scanner.nextLine();
+
+            if(login.equals(loginDigitado) && password.equals(passwordDigitado)){
+                System.out.println("ACCESO CONCEDIDO");
+                exibirTelaLogin = false;
+            }else{
+                System.out.println("ACESSO NEGADO");
+            }
+        }
+    }
+}
