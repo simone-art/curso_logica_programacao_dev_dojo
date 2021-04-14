82a3a7e09809fd6916744e994bb7da93546e3801
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-14 15:57:27 -0300

    "Criado código usando o for para calcular quadrados de um número de una lista que começa no 15 e termina no 200 "

---
+++ src/main/java/academy/devdojo/lacosderepeticao/fors/Aula02Exercicio01.java
@@ -0,0 +1,12 @@
+package academy.devdojo.lacosderepeticao.fors;
+/*
+Fazer código que apresente de uma lista de números que começa no 15 até 200, quais números são quadrados
+Números quadrados: números que ao ser multiplicados por ele mesmo geram um número positivo
+ */
+public class Aula02Exercicio01 {
+    public static void main(String[] args) {
+        for (int i = 15; i < 200; i++) {
+            System.out.println(i * i);
+        }
+    }
+}
