887fb997582f827de9e2819a5359bdc7228da551
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-14 17:51:21 -0300

    "Criado código para mostrar tabuada ao usuário digitar número"

---
+++ src/main/java/academy/devdojo/lacosderepeticao/fors/Aula02Exercicio02.java
@@ -0,0 +1,18 @@
+package academy.devdojo.lacosderepeticao.fors;
+
+import java.util.Scanner;
+
+/*
+Construir tabuada com número digitado pelo usuário
+ */
+public class Aula02Exercicio02 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite um número para mostrar a Tabuada: ");
+        int numeroTabuada = scanner.nextInt();
+        for (int i = 1; i < 11; i++) {
+            int resultado = (i * numeroTabuada);
+            System.out.println(i + " x " + numeroTabuada + " = " + resultado);
+        }
+    }
+}
