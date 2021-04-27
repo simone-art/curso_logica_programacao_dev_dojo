f9298d7ddf81d8ada372543e2aefac1a64adad9e
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-27 17:17:26 -0300

    "Código introdutório de Arrays com vários for"

--- src/main/java/academy/devdojo/arrays/Aula02ArraysComFor.java
+++ src/main/java/academy/devdojo/arrays/Aula02ArraysComFor.java
@@ -6,13 +6,21 @@ public class Aula02ArraysComFor {
     public static void main(String[] args) {
         double[] notas = new double[4];
         Scanner scanner = new Scanner(System.in);
+        //Laço de repeticão pra preencher os valores digitados pelo usuário
         for(int i = 0; i < notas.length; i++){
             System.out.println("Digite a sua nota: " +(i+1));
             notas[i] = scanner.nextDouble();
         }
 
+        double mediaNotas = 0;
+
+        //laço pra imprimir os valores
         for (int i = 0; i < notas.length ; i++) {
+            mediaNotas = mediaNotas + notas[i];
             System.out.println("Nota " + (i + 1) + ": " + notas[i]);
         }
+        //Cálculo da média
+        mediaNotas = mediaNotas/notas.length;
+        System.out.println("A media das notas é: " + mediaNotas);
     }
 }
