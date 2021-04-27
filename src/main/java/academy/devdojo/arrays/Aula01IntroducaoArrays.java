9380262a3387ffc203cb5957ae9434d0201020ad
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-04-27 16:16:11 -0300

    "Código introdutório de Arrays"

---
+++ src/main/java/academy/devdojo/arrays/Aula01IntroducaoArrays.java
@@ -0,0 +1,25 @@
+package academy.devdojo.arrays;
+
+public class Aula01IntroducaoArrays {
+    public static void main(String[] args) {
+        double nota1 = 7.5;
+        double nota2 = 8.0;
+        double nota3 = 3.0;
+        double nota4 = 5.0;
+
+        double media = (nota1 + nota2 + nota3 +nota4)/3;
+        System.out.println(media);
+        System.out.println("============================");
+        //Declaração de Arrays
+        double[] notas = new double[4];
+        notas[0] = 7.5;
+        notas[1] = 8.0;
+        notas[2] = 3.0;
+        notas[3] = 5.0;
+
+        System.out.println(notas[0]);
+        System.out.println(notas[1]);
+        System.out.println(notas[2]);
+        System.out.println(notas[3]);
+    }
+}
