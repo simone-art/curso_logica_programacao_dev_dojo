e32923c5aef71a6c97491ce02ff3b85cb0bccaaf
s.santos.pimenta <simoneperiodista3@gmail.com>
2021-05-09 17:16:52 -0300

    "Refactorado código sobre arrays multidimensionais parte II"

--- src/main/java/academy/devdojo/arrays/multidimensional/Aula02InicializandoArraysMultidimensionais.java
+++ src/main/java/academy/devdojo/arrays/multidimensional/Aula02InicializandoArraysMultidimensionais.java
@@ -1,17 +1,28 @@
 package academy.devdojo.arrays.multidimensional;
 
+import java.util.Scanner;
+
 public class Aula02InicializandoArraysMultidimensionais {
     public static void main(String[] args) {
         int [][] arrayMulti = new int[2][3];
-        arrayMulti[0][0] = 22;
-        arrayMulti[0][1] = 11;
-        arrayMulti[0][2] = 54;
-        arrayMulti[1][0] = 44;
-        arrayMulti[1][1] = 55;
-        arrayMulti[1][2] = 10;
+//        arrayMulti[0][0] = 22;
+//        arrayMulti[0][1] = 11;
+//        arrayMulti[0][2] = 54;
+//        arrayMulti[1][0] = 44;
+//        arrayMulti[1][1] = 55;
+//        arrayMulti[1][2] = 10;
+        Scanner scanner = new Scanner(System.in);
         for (int i = 0; i < arrayMulti.length; i++) {
             for (int j = 0; j < arrayMulti[i].length; j++) {
+                System.out.println("Digite o valor da posição ["+i+"] ["+j+"]");
                 System.out.println("["+i+"] ["+j+"] =" + arrayMulti[i][j]);
+                arrayMulti[i][j] = scanner.nextInt();
+            }
+        }
+        for (int i = 0; i < arrayMulti.length; i++) {
+            for (int j = 0; j < arrayMulti[i].length; j++) {
+                System.out.println(" + ["+ i + "] ["+ j + "]= " +arrayMulti[i][j]);
+
             }
         }
     }
